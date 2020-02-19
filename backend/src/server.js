f7534e562f25d9799ecbd5cbf1cbde9056fb4d66
unknown <mateus.amaral018@gmail.com>
2020-02-19 16:14:34 -0300

    adicionado postgres e finalizado a aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,17 @@ const app = express();
 
 app.use(express.static('../frontend/public'));
 
+app.use(express.urlencoded({ extended: true }));
+const { Pool } = require('pg');
+
+const db = new Pool({
+  user: 'postgres',
+  password: 'root',
+  host: 'localhost',
+  port: 5432,
+  database: 'doe',
+});
+
 const nunjucks = require('nunjucks');
 
 nunjucks.configure('../frontend', {
@@ -11,25 +22,30 @@ nunjucks.configure('../frontend', {
   noCache: true,
 });
 
-const donors = [
-  {
-    name: 'Mateus',
-    blood: 'AB+',
-  },
-  {
-    name: 'Geisa',
-    blood: 'B+',
-  },
-  {
-    name: 'Cleiton',
-    blood: 'A+',
-  },
-  {
-    name: 'Erick',
-    blood: 'O+',
-  },
-];
-app.get('/', (req, res) => res.render('index.html', { donors }));
+app.get('/', (req, res) => {
+  db.query('SELECT * FROM "donors"', (err, result) => {
+    if (err) {
+      return res.send('erro de banco de dados 1');
+    }
+
+    const donors = result.rows;
+    return res.render('index.html', { donors });
+  });
+});
+
+
+app.post('/', (req, res) => {
+  const { name, blood, email } = req.body;
+  if (name === '' || email === '' || blood === '') {
+    return res.send('Todos os campos são obrigatórios');
+  }
+  const query = 'INSERT INTO donors("name", "blood", "email") VALUES($1, $2, $3)';
+  const values = [name, blood, email];
+  db.query(query, values, (err) => {
+    if (err) return res.send('Por favor verifique o que foi digitado no formulário de ajuda');
+    return res.redirect('/');
+  });
+});
 
 app.listen(3333, () => {
   console.log('server is runnig');
